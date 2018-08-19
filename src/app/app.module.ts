3c2b20d56cb47352f06ecbf9af7f1106f8d73a8a
Cristian Predator <xthecapx@gmail.com>
2018-08-19 14:08:47 -0500

    Exercise two description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatCardModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyMaterialNavComponent } from './my-material-nav/my-material-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { DearPuppyComponent } from './dear-puppy/dear-puppy.component';
+
+const appRoutes: Routes = [
+  { path: 'first-page', component: FirstComponent },
+  { path: 'second-page', component: SecondComponent },
+  { path: 'dear-puppy', component: DearPuppyComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyMaterialNavComponent
+    MyMaterialNavComponent,
+    FirstComponent,
+    SecondComponent,
+    DearPuppyComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     MatCardModule,
     MatButtonModule,
     LayoutModule,
